427d85088dace7c4d85d7d501dfd447eebfe2f6a
jacob <jakeyog@gmail.com>
2023-02-01 13:27:52 -0800

    le dimos funcionalidad el iconito de rayitas en mobile para que aparecieray desapareciera al darle click, asi mismo aniadimos algunas medias queries en css

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-alert("hola mundo");
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuIconito = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDeskMenu);
+menuIconito.addEventListener('click', toggleMobileMenu);
 
+
+// Funcion para que funcione el iconito
+
+function toggleMobileMenu(){
+mobileMenu.classList.toggle('inactive')
+}
+
+// funcion para aparecer y desaparecer el menu
 function toggleDeskMenu(){
     desktopMenu.classList.toggle('inactive')
 }
